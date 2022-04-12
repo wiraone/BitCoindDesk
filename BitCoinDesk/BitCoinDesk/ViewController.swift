0e4c586636c166741435f2cf8fedf2f2b2682e14
wirawan <xnamine@gmail.com>
2022-04-13 01:45:38 +0700

    Setup initial project
    Init project
    Add image asset
    Create MVP Folder Structure

---
+++ BitCoinDesk/BitCoinDesk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitCoinDesk
+//
+//  Created by Mac on 13/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
